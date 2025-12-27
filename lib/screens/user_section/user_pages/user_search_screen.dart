6ac85a2453248d150f35f45deb7d7e338dbd06dd
fwdAndroid <fwdkaleem@gmail.com>
2025-12-27 17:38:15 +0500

    Search

--- lib/screens/user_section/user_pages/user_search_screen.dart
+++ lib/screens/user_section/user_pages/user_search_screen.dart
@@ -10,6 +10,22 @@ class UserSearchScreen extends StatefulWidget {
 class _UserSearchScreenState extends State<UserSearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Icon(Icons.search, size: 100, color: Colors.grey[400])),
+          const SizedBox(height: 20),
+          Center(
+            child: Text("Search Result not found",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey[600],
+                    fontWeight: FontWeight.w500)),
+          ),
+        ],
+      ),
+    );
   }
 }
